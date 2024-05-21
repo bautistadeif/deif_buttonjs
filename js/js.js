7c83afd7edb2e79ece72b8fbd1d28b1bf89e2827
bautistadeif <deifrich99@gmail.com>
2024-05-21 16:43:52 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,11 @@
+function changecolor(){
+    document.getElementById("123").style.color="#ccceee";
+}
+function changeimage(){
+    document.getElementById('123').src="img/cat.gif";
+}
+function changetxt{
+    var txt= "";
+    txt =document.getElementById("input1").ariaValueMax;
+    document.getElementById("123").innerHTML = txt; 
+}
